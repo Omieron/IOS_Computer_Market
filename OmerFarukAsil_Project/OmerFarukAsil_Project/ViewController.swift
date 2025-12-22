fa4512728955e18814eb3d01050bad9cb339fcbb
Ömer Faruk Asil <asilo02@hotmail.com>
2025-12-22 21:22:33 +0300

    UI of basic part

---
+++ OmerFarukAsil_Project/OmerFarukAsil_Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OmerFarukAsil_Project
+//
+//  Created by Ömer Faruk Asil on 22.12.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
