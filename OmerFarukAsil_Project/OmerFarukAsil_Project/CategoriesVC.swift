328dcd3480f65ef8863688b44abe2f6303b095d4
Ömer Faruk Asil <asilo02@hotmail.com>
2025-12-24 16:10:41 +0300

    update of collectionview of categories

--- OmerFarukAsil_Project/OmerFarukAsil_Project/CategoriesVC.swift
+++ OmerFarukAsil_Project/OmerFarukAsil_Project/CategoriesVC.swift
@@ -9,9 +9,22 @@ import UIKit
 
 class CategoriesVC: UIViewController {
 
+    @IBOutlet weak var gamingCollectionView: UICollectionView!
+    
+    
+    @IBOutlet weak var officeCollectionView: UICollectionView!
+    
+    
+    @IBOutlet weak var workstationCollectionView: UICollectionView!
+    
+    let xmlCategoryData = DataSoruce()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.xmlCategoryData.populateFromXML()
+        self.gamingCollectionView.reloadData()
+        self.officeCollectionView.reloadData()
+        self.workstationCollectionView.reloadData()
         // Do any additional setup after loading the view.
     }
     
@@ -26,4 +39,48 @@ class CategoriesVC: UIViewController {
     }
     */
 
+}
+
+extension CategoriesVC: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        numberOfItemsInSection section: Int) -> Int {
+
+        if collectionView == gamingCollectionView {
+            return xmlCategoryData.rootCategories[0].children.count
+        } else if collectionView == officeCollectionView {
+            return xmlCategoryData.rootCategories[1].children.count
+        } else {
+            return xmlCategoryData.rootCategories[2].children.count
+        }
+    }
+
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+
+        let cell = collectionView.dequeueReusableCell(
+            withReuseIdentifier: "SubCategoryCell",
+            for: indexPath
+        ) as! CustomCategoriesCollectionViewCell
+
+        let category: Category
+
+        if collectionView == gamingCollectionView {
+            category = xmlCategoryData.rootCategories[0].children[indexPath.item]
+        } else if collectionView == officeCollectionView {
+            category = xmlCategoryData.rootCategories[1].children[indexPath.item]
+        } else {
+            category = xmlCategoryData.rootCategories[2].children[indexPath.item]
+        }
+
+        cell.titleLabel.text = category.name.capitalized
+        return cell
+    }
+
+    
 }
---
+++ OmerFarukAsil_Project/OmerFarukAsil_Project/CustomCategoriesCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CustomCategoriesCollectionViewCell.swift
+//  OmerFarukAsil_Project
+//
+//  Created by Ömer Faruk Asil on 24.12.2025.
+//
+
+import UIKit
+
+class CustomCategoriesCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    
+}
